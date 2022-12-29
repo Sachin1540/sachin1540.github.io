5afe877b06b4d735115a5080c39b357fe2f80b20
Sachin <sd@gmail.com>
2022-12-29 16:11:54 +0530

    Updates

---
+++ precache-manifest.76821fca98afda9358a73289ac6bf340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3663addab43d03f5e9d2cbc471916e49",
+    "url": "/react-client-project/index.html"
+  },
+  {
+    "revision": "b8066781b3c2c6d8ff72",
+    "url": "/react-client-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "32c1fee5ba355e0c5d0f",
+    "url": "/react-client-project/static/js/2.bc58cef7.chunk.js"
+  },
+  {
+    "revision": "f7c97ece1c84f40b0bfa44506f83499c",
+    "url": "/react-client-project/static/js/2.bc58cef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8066781b3c2c6d8ff72",
+    "url": "/react-client-project/static/js/main.444343e4.chunk.js"
+  },
+  {
+    "revision": "9b044039f9d1d6d74133",
+    "url": "/react-client-project/static/js/runtime-main.5879f22d.js"
+  }
+]);
